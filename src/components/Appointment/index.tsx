9393d092cdaee1d90b5cdfcbb8c56bdda637fd4b
sleduardo20 <sl.eduardo20@gmail.com>
2021-06-12 16:14:18 -0300

    ✨ feat (Appointment): create component

---
+++ src/components/Appointment/index.tsx
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+
+import { format } from 'date-fns';
+
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
+import { Flex, Heading, Stack, Text, Button, Image } from '@chakra-ui/react';
+import { useEffect } from 'react';
+
+export const Appointment = () => {
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    const dataTest = format(date, 'yyyy-MM-dd');
+    console.log(dataTest);
+  }, [date]);
+
+  return (
+    <Stack
+      background="gray.800"
+      spacing={{ base: 8, md: 24 }}
+      height="100vh"
+      pl="8"
+      pr="8"
+      align="center"
+    >
+      <Flex direction="column" align="center" h="100%">
+        <Stack
+          textAlign="center"
+          align="center"
+          spacing={{ base: 8, md: 10 }}
+          py={{ base: 20, md: 28 }}
+        >
+          <Heading
+            fontWeight={600}
+            fontSize={{ base: '3xl', sm: '6xl', md: '8xl' }}
+            lineHeight="110%"
+            color="white"
+          >
+            Agendamento
+            <Text as="span" color="orange.400">
+              Fácil
+            </Text>
+          </Heading>
+          <Text color="gray.300" maxW="3xl" fontSize={24}>
+            Faça o agendamendo de seus compromissos e fique tranquilo.
+          </Text>
+        </Stack>
+
+        <Stack spacing={32} direction="row" marginTop={14}>
+          <Calendar onChange={setDate} value={date} />
+          <Button
+            rounded="full"
+            px={12}
+            height={16}
+            colorScheme="orange"
+            bg="orange.400"
+            _hover={{ bg: 'orange.500' }}
+          >
+            Agendar
+          </Button>
+        </Stack>
+      </Flex>
+    </Stack>
+  );
+};
